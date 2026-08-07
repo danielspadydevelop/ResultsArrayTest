4a06aa7acf40ddb4941d2193f4ca18898d620d9d
Daniel Spady <d.spady@tegna.com>
2026-08-07 15:12:50 -0400

    Initial Commit

---
+++ ResultsArrayTest/ResultsArrayTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResultsArrayTestApp.swift
+//  ResultsArrayTest
+//
+//  Created by Daniel Spady on 8/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ResultsArrayTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
